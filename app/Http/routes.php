9475b9c77daa2c914fb1e2aa14c89c3e90f8e2f0
chaos29092 <chaos29092@gmail.com>
2016-03-22 15:37:04 +0800

    login system done admin route done

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,50 +12,52 @@
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('index');
 });
-Route::get('products',function(){
+Route::get('/products',function(){
     return view('products.category');
 });
-Route::get('products/test',function(){
+Route::get('/products/test',function(){
     return view('products.category_in');
 });
-Route::get('products/testp',function(){
+Route::get('/products/testp',function(){
     return view('products.productf');
 });
-Route::get('products/testt',function(){
+Route::get('/products/testt',function(){
     return view('products.productt');
 });
-Route::get('contact-service',function(){
+Route::get('/contact-service',function(){
     return view('contact_service');
 });
-Route::get('company',function(){
+Route::get('/company',function(){
     return view('company');
 });
-Route::get('news',function(){
+Route::get('/news',function(){
     return view('news.list');
 });
-Route::get('news/test',function(){
+Route::get('/news/test',function(){
     return view('news.news_detals');
 });
-Route::get('news/newsletter',function(){
+Route::get('/news/newsletter',function(){
     return view('news.newsletter');
 });
-Route::get('news/events',function(){
+Route::get('/news/events',function(){
     return view('news.dates_events');
 });
-Route::get('downloads',function(){
+Route::get('/downloads',function(){
     return view('downloads');
 });
-Route::get('downloads/operating',function(){
+Route::get('/downloads/operating',function(){
     return view('downloads_operating');
 });
-Route::get('downloads/brochures',function(){
+Route::get('/downloads/brochures',function(){
     return view('downloads_brochures');
 });
-Route::get('downloads/miscellaneous',function(){
+Route::get('/downloads/miscellaneous',function(){
     return view('downloads_miscellaneous');
 });
+
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -67,6 +69,8 @@ Route::get('downloads/miscellaneous',function(){
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/admin', 'ProductController@index');
 });
