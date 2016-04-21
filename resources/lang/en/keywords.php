8c3532ee5982a4957f25a5ac6ec10e10c0c6c244
chaos29092 <chaos29092@gmail.com>
2016-04-21 14:22:21 +0800

    keywords done

--- resources/lang/en/keywords.php
+++ resources/lang/en/keywords.php
@@ -8,44 +8,44 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'index_title' => '',
-    'index_description' => '',
-    'index_keywords' => '',
+    'index_title' => 'Laboratory and Industrial Furnaces',
+    'index_description' => 'We supply muffle furnace, tube furnace, CVD & PECVD Systems, Vacuum Furnace & Atmosphere Furnace ,dental furnace, and Can be customized according to your needs.',
+    'index_keywords' => 'muffle furnace, tube furnace, CVD & PECVD Systems, Vacuum Furnace & Atmosphere Furnace, dental furnace',
 //    /company
-    'company_title' => '',
-    'company_description' => '',
-    'company_keywords' => '',
+    'company_title' => 'Company History & Profile',
+    'company_description' => 'The Kejia Furnace brand is synonymous with high quality, leading heat technology in the design and manufacture of laboratory and industrial furnaces',
+    'company_keywords' => 'Kejia Company History & Profile',
 //    /contact-service
-    'contact_title' => '',
-    'contact_description' => '',
-    'contact_keywords' => '',
+    'contact_title' => 'Contact and Service',
+    'contact_description' => 'If you need lab furnace, please contact us',
+    'contact_keywords' => 'Kejia Furnace Contact and Service',
 //    /downloads
-    'downloads_title' => '',
-    'downloads_description' => '',
-    'downloads_keywords' => '',
+    'downloads_title' => 'Downloads',
+    'downloads_description' => 'All important documents are available for download here.',
+    'downloads_keywords' => 'Kejia Furnace Downloads',
 //    /downloads/operating
-    'operating_title' => '',
-    'operating_description' => '',
-    'operating_keywords' => '',
+    'operating_title' => 'Downloads - Operating',
+    'operating_description' => 'Kejia Furnace manuals',
+    'operating_keywords' => 'Kejia Furnace manuals',
 //    /downloads/brochures
-    'brochures_title' => '',
-    'brochures_description' => '',
-    'brochures_keywords' => '',
+    'brochures_title' => 'Downloads - Brochures',
+    'brochures_description' => 'Kejia Furnace Brochures',
+    'brochures_keywords' => 'Kejia Furnace Brochures',
 //    /downloads/miscellaneous
-    'miscellaneous_title' => '',
-    'miscellaneous_description' => '',
-    'miscellaneous_keywords' => '',
+    'miscellaneous_title' => 'Downloads - Miscellaneous',
+    'miscellaneous_description' => 'Kejia Furnace Miscellaneous',
+    'miscellaneous_keywords' => 'Kejia Furnace Miscellaneous',
 //    /products
-    'products_title' => '',
-    'products_description' => '',
-    'products_keywords' => '',
+    'products_title' => 'All Products',
+    'products_description' => 'We supply muffle furnace, tube furnace, CVD & PECVD Systems, Vacuum Furnace & Atmosphere Furnace ,dental furnace, and Can be customized according to your needs.',
+    'products_keywords' => 'muffle furnace, tube furnace, CVD & PECVD Systems, Vacuum Furnace & Atmosphere Furnace, dental furnace',
 //    /news
-    'news_title' => '',
-    'news_description' => '',
-    'news_keywords' => '',
+    'news_title' => 'News',
+    'news_description' => 'Kejia Furnace News',
+    'news_keywords' => 'Kejia Furnace News',
 //    /news/cases
-    'cases_title' => '',
-    'cases_description' => '',
-    'cases_keywords' => '',
+    'cases_title' => 'Custom Cases',
+    'cases_description' => 'Kejia Furnace Custom Cases',
+    'cases_keywords' => 'Kejia Furnace Custom Cases',
 
 ];
